66b0a494b796d60de1f7450eec7dfc089e3163aa
Facenapalm <Antik125@ya.ru>
2023-04-15 21:13:59 +0300

    Rewrite ogdb_extract_country.py

--- ogdb_extract_country.py
+++ ogdb_extract_country.py
@@ -19,185 +19,179 @@
 # SOFTWARE.
 
 """
-Extract Country of origin (P495) based on OGDB ID (P7564).
+Add Country of origin (P495) based on OGDB ID (P7564).
 
 See also:
 
    https://www.wikidata.org/wiki/Wikidata:Database_reports/Constraint_violations/P7564#Item_P495
 """
 
-import pywikibot
-from pywikibot import pagegenerators as pg
-import urllib.request
-import time
-import sys
 import re
-import random
-from datetime import datetime
+import requests
+import pywikibot
+from argparse import ArgumentParser
+from common.utils import get_current_wbtime, parse_input_source
 
-QUERY = """
-SELECT DISTINCT ?item {
-    ?item p:P7564 []
-    FILTER NOT EXISTS { ?item p:P495 [] }
-}
-"""
+class OGDBBot():
+    def __init__(self):
+        self.repo = pywikibot.Site()
+        self.repo.login()
 
-def get_current_wbtime():
-    timestamp = datetime.utcnow()
-    return pywikibot.WbTime(year=timestamp.year, month=timestamp.month, day=timestamp.day)
+        get_item = lambda x: pywikibot.ItemPage(self.repo, x)
+        self.ogdb_item = get_item("Q60315954")
+        self.country_items = {
+            "Kanada": get_item("Q16"),
+            "Japan": get_item("Q17"),
+            "Norwegen": get_item("Q20"),
+            "England": get_item("Q21"),
+            "Schottland": get_item("Q22"),
+            "Wales": get_item("Q25"),
+            "Nordirland": get_item("Q26"),
+            "Ungarn": get_item("Q28"),
+            "Spanien": get_item("Q29"),
+            "USA": get_item("Q30"),
+            "Belgien": get_item("Q31"),
+            "Luxemburg": get_item("Q32"),
+            "Finnland": get_item("Q33"),
+            "Schweden": get_item("Q34"),
+            "Dänemark": get_item("Q35"),
+            "Polen": get_item("Q36"),
+            "Litauen": get_item("Q37"),
+            "Italien": get_item("Q38"),
+            "Schweiz": get_item("Q39"),
+            "Österreich": get_item("Q40"),
+            "Griechenland": get_item("Q41"),
+            "Türkei": get_item("Q43"),
+            "Portugal": get_item("Q45"),
+            "Niederlande": get_item("Q55"),
+            "Uruguay": get_item("Q77"),
+            "Mexiko": get_item("Q96"),
+            "Frankreich": get_item("Q142"),
+            "China": get_item("Q148"),
+            "Brasilien": get_item("Q155"),
+            "Russland": get_item("Q159"),
+            "Deutschland": get_item("Q183"),
+            "Belarus": get_item("Q184"),
+            "Island": get_item("Q189"),
+            "Estland": get_item("Q191"),
+            "Lettland": get_item("Q211"),
+            "Ukraine": get_item("Q212"),
+            "Tschechien": get_item("Q213"),
+            "Slowakei": get_item("Q214"),
+            "Slowenien": get_item("Q215"),
+            "Moldawien": get_item("Q217"),
+            "Rumänien": get_item("Q218"),
+            "Bulgarien": get_item("Q219"),
+            "Kroatien": get_item("Q224"),
+            "Zypern": get_item("Q229"),
+            "Georgien": get_item("Q230"),
+            "Kasachstan": get_item("Q232"),
+            "Malta": get_item("Q233"),
+            "Indonesien": get_item("Q252"),
+            "Südafrika": get_item("Q258"),
+            "Chile": get_item("Q298"),
+            "Singapur": get_item("Q334"),
+            "Serbien": get_item("Q403"),
+            "Australien": get_item("Q408"),
+            "Argentinien": get_item("Q414"),
+            "Nordkorea": get_item("Q423"),
+            "Neuseeland": get_item("Q664"),
+            "Indien": get_item("Q668"),
+            "Venezuela": get_item("Q717"),
+            "Kolumbien": get_item("Q739"),
+            "El Salvador": get_item("Q792"),
+            "Iran": get_item("Q794"),
+            "Costa Rica": get_item("Q800"),
+            "Israel": get_item("Q801"),
+            "Malaysia": get_item("Q833"),
+            "Taiwan": get_item("Q865"),
+            "Thailand": get_item("Q869"),
+            "Vietnam": get_item("Q881"),
+            "Libanon": get_item("Q882"),
+            "Südkorea": get_item("Q884"),
+            "Philippinen": get_item("Q928"),
+            "Marokko": get_item("Q1028"),
+            "Hong Kong": get_item("Q8646"),
+            "U.d.S.S.R.": get_item("Q15180"),
+            "Irland": get_item("Q22890"),
+        }
 
-def get_country(ogdb_id):
-    attempts = 3
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-        "Accept-Encoding": "none",
-        "Accept-Language": "en-US,en;q=0.8",
-        "Connection": "keep-alive"
-    }
-    url = "https://ogdb.eu/index.php?section=title&titleid={}".format(ogdb_id)
-    for attempt_no in range(attempts):
+    def get_countries(self, ogdb_id):
+        headers = {
+            "User-Agent": "Wikidata bot"
+        }
+        response = requests.get(f"https://ogdb.eu/index.php?section=title&titleid={ogdb_id}")
+        if not response:
+            raise RuntimeError(f"can't download game entry {ogdb_id}")
+        html = response.text
+
+        if "Keinen Titel zu dieser TitleId gefunden!" in html:
+            raise RuntimeError(f"no game entry {ogdb_id} exist")
+
+        # <tr>
+        # <td class="tboldc" width="140">&nbsp; Herkunftsland:</td>
+        # 
+        # <td class="tnormg" width="*">&nbsp;England</td>
+        # </tr>
+        match = re.search(r"<td class=\"tboldc\" width=\"140\">\&nbsp; Herkunftsland:</td>\s+<td class=\"tnormg\" width=\"\*\">\&nbsp;([^<]+)</td>", html)
+        if not match:
+            raise RuntimeError(f"no country of origin specified for game entry {ogdb_id}")
+        countries = match.group(1)
+
+        result = []
+        for country_name in countries.split(", "):
+            if country_name not in self.country_items:
+                raise RuntimeError(f"unknown country `{country_name}`")
+            result.append(self.country_items[country_name])
+
+        return result
+
+    def process_item(self, item):
         try:
-            # time.sleep(random.randint(1, 3))
-            request = urllib.request.Request(url, None, headers)
-            response = urllib.request.urlopen(request)
-            html = response.read().decode("utf-8")
-        except Exception as error:
-            if attempt_no == (attempts - 1):
-                raise error
+            if "P495" in item.claims:
+                raise RuntimeError("country of origin already set")
+            if "P7564" not in item.claims:
+                raise RuntimeError("OGDB ID not found")
+            if len(item.claims["P7564"]) > 1:
+                raise RuntimeError("several OGDB IDs found")
+            ogdb_id = item.claims["P7564"][0].getTarget()
 
-    # <tr>
-    # <td class="tboldc" width="140">&nbsp; Herkunftsland:</td>
-    # 
-    # <td class="tnormg" width="*">&nbsp;England</td>
-    # </tr>
-    match = re.search(r"<td class=\"tboldc\" width=\"140\">\&nbsp; Herkunftsland:</td>\s+<td class=\"tnormg\" width=\"\*\">\&nbsp;(.*?)</td>", html)
-    if match:
-        return match.group(1)
-    else:
-        return ""
+            for country in self.get_countries(ogdb_id):
+                claim = pywikibot.Claim(self.repo, "P495")
+                claim.setTarget(country)
 
-def main():
-    repo = pywikibot.Site()
-    repo.login()
+                statedin = pywikibot.Claim(self.repo, "P248")
+                statedin.setTarget(self.ogdb_item)
+                source_id = pywikibot.Claim(self.repo, "P7564")
+                source_id.setTarget(ogdb_id)
+                retrieved = pywikibot.Claim(self.repo, "P813")
+                retrieved.setTarget(get_current_wbtime())
+                claim.addSources([statedin, source_id, retrieved], summary="Adding OGDB as a source.")
 
-    OGDB = pywikibot.ItemPage(repo, "Q60315954")
-    country_items = {
-        "Kanada": pywikibot.ItemPage(repo, "Q16"),
-        "Japan": pywikibot.ItemPage(repo, "Q17"),
-        "Norwegen": pywikibot.ItemPage(repo, "Q20"),
-        "England": pywikibot.ItemPage(repo, "Q21"),
-        "Schottland": pywikibot.ItemPage(repo, "Q22"),
-        "Wales": pywikibot.ItemPage(repo, "Q25"),
-        "Nordirland": pywikibot.ItemPage(repo, "Q26"),
-        "Ungarn": pywikibot.ItemPage(repo, "Q28"),
-        "Spanien": pywikibot.ItemPage(repo, "Q29"),
-        "USA": pywikibot.ItemPage(repo, "Q30"),
-        "Belgien": pywikibot.ItemPage(repo, "Q31"),
-        "Luxemburg": pywikibot.ItemPage(repo, "Q32"),
-        "Finnland": pywikibot.ItemPage(repo, "Q33"),
-        "Schweden": pywikibot.ItemPage(repo, "Q34"),
-        "Dänemark": pywikibot.ItemPage(repo, "Q35"),
-        "Polen": pywikibot.ItemPage(repo, "Q36"),
-        "Litauen": pywikibot.ItemPage(repo, "Q37"),
-        "Italien": pywikibot.ItemPage(repo, "Q38"),
-        "Schweiz": pywikibot.ItemPage(repo, "Q39"),
-        "Österreich": pywikibot.ItemPage(repo, "Q40"),
-        "Griechenland": pywikibot.ItemPage(repo, "Q41"),
-        "Türkei": pywikibot.ItemPage(repo, "Q43"),
-        "Portugal": pywikibot.ItemPage(repo, "Q45"),
-        "Niederlande": pywikibot.ItemPage(repo, "Q55"),
-        "Uruguay": pywikibot.ItemPage(repo, "Q77"),
-        "Mexiko": pywikibot.ItemPage(repo, "Q96"),
-        "Frankreich": pywikibot.ItemPage(repo, "Q142"),
-        "China": pywikibot.ItemPage(repo, "Q148"),
-        "Brasilien": pywikibot.ItemPage(repo, "Q155"),
-        "Russland": pywikibot.ItemPage(repo, "Q159"),
-        "Deutschland": pywikibot.ItemPage(repo, "Q183"),
-        "Belarus": pywikibot.ItemPage(repo, "Q184"),
-        "Island": pywikibot.ItemPage(repo, "Q189"),
-        "Estland": pywikibot.ItemPage(repo, "Q191"),
-        "Lettland": pywikibot.ItemPage(repo, "Q211"),
-        "Ukraine": pywikibot.ItemPage(repo, "Q212"),
-        "Tschechien": pywikibot.ItemPage(repo, "Q213"),
-        "Slowakei": pywikibot.ItemPage(repo, "Q214"),
-        "Slowenien": pywikibot.ItemPage(repo, "Q215"),
-        "Moldawien": pywikibot.ItemPage(repo, "Q217"),
-        "Rumänien": pywikibot.ItemPage(repo, "Q218"),
-        "Bulgarien": pywikibot.ItemPage(repo, "Q219"),
-        "Kroatien": pywikibot.ItemPage(repo, "Q224"),
-        "Zypern": pywikibot.ItemPage(repo, "Q229"),
-        "Georgien": pywikibot.ItemPage(repo, "Q230"),
-        "Kasachstan": pywikibot.ItemPage(repo, "Q232"),
-        "Malta": pywikibot.ItemPage(repo, "Q233"),
-        "Indonesien": pywikibot.ItemPage(repo, "Q252"),
-        "Südafrika": pywikibot.ItemPage(repo, "Q258"),
-        "Chile": pywikibot.ItemPage(repo, "Q298"),
-        "Singapur": pywikibot.ItemPage(repo, "Q334"),
-        "Serbien": pywikibot.ItemPage(repo, "Q403"),
-        "Australien": pywikibot.ItemPage(repo, "Q408"),
-        "Argentinien": pywikibot.ItemPage(repo, "Q414"),
-        "Nordkorea": pywikibot.ItemPage(repo, "Q423"),
-        "Neuseeland": pywikibot.ItemPage(repo, "Q664"),
-        "Indien": pywikibot.ItemPage(repo, "Q668"),
-        "Venezuela": pywikibot.ItemPage(repo, "Q717"),
-        "Kolumbien": pywikibot.ItemPage(repo, "Q739"),
-        "El Salvador": pywikibot.ItemPage(repo, "Q792"),
-        "Iran": pywikibot.ItemPage(repo, "Q794"),
-        "Costa Rica": pywikibot.ItemPage(repo, "Q800"),
-        "Israel": pywikibot.ItemPage(repo, "Q801"),
-        "Malaysia": pywikibot.ItemPage(repo, "Q833"),
-        "Taiwan": pywikibot.ItemPage(repo, "Q865"),
-        "Thailand": pywikibot.ItemPage(repo, "Q869"),
-        "Vietnam": pywikibot.ItemPage(repo, "Q881"),
-        "Libanon": pywikibot.ItemPage(repo, "Q882"),
-        "Südkorea": pywikibot.ItemPage(repo, "Q884"),
-        "Philippinen": pywikibot.ItemPage(repo, "Q928"),
-        "Marokko": pywikibot.ItemPage(repo, "Q1028"),
-        "Hong Kong": pywikibot.ItemPage(repo, "Q8646"),
-        "U.d.S.S.R.": pywikibot.ItemPage(repo, "Q15180"),
-        "Irland": pywikibot.ItemPage(repo, "Q22890"),
-    }
-    generator = pg.WikidataSPARQLPageGenerator(QUERY, site=repo)
-    for item in generator:
-        if "P495" in item.claims:
-            print("{}: country already set".format(item.title()))
-            continue
-        if "P7564" not in item.claims:
-            print("{}: OGDB ID not found".format(item.title()))
-            continue
-        if len(item.claims["P7564"]) > 1:
-            print("{}: several OGDB IDs found".format(item.title()))
-            continue
-        ogdb_id = item.claims["P7564"][0].getTarget()
-        countries = get_country(ogdb_id)
-        if not countries:
-            print("{}: can't get country for `{}`".format(item.title(), ogdb_id))
-            continue
-        countries = countries.split(", ")
-        skip = False
-        for country in countries:
-            if country not in country_items:
-                print("{}: unknown country `{}`".format(item.title(), country))
-                skip = True
-        if skip:
-            continue
+                item.addClaim(claim, summary="Add country of origin based on OGDB database.")
 
-        for country in countries:
-            claim = pywikibot.Claim(repo, "P495")
-            claim.setTarget(country_items[country])
+                if "en" in country.labels:
+                    country_label = country.labels["en"]
+                else:
+                    country_label = country.title()
+                print(f"{item.title()}: added country `{country_label}`")
+        except RuntimeError as error:
+            print(f"{item.title()}: {error}")
 
-            statedin = pywikibot.Claim(repo, "P248")
-            statedin.setTarget(OGDB)
-            source_id = pywikibot.Claim(repo, "P7564")
-            source_id.setTarget(ogdb_id)
-            retrieved = pywikibot.Claim(repo, 'P813')
-            retrieved.setTarget(get_current_wbtime())
-            claim.addSources([statedin, source_id, retrieved], summary="Adding OGDB as a source.")
+    def run(self):
+        parser = ArgumentParser(description="Add Country of origin (P495) based on OGDB ID (P7564).")
+        parser.add_argument("input", nargs="?", default="all", help="either a path to the file with the list of IDs of items to process (Qnnn) or a keyword \"all\"; treated as \"all\" by default")
+        args = parser.parse_args()
 
-            item.addClaim(claim, summary="Add country of origin based on OGDB database.")
-            print("{}: added country `{}`".format(item.title(), country))
+        query = """
+            SELECT ?item {
+                ?item p:P7564 [] .
+                FILTER NOT EXISTS { ?item p:P495 [] }
+            }
+        """
+
+        for item in parse_input_source(self.repo, args.input, query):
+            self.process_item(item)
 
 if __name__ == "__main__":
-    main()
+    OGDBBot().run()
